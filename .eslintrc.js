9441056a4c9cdbf3e0637bfdf65211983d607180
Annie <anniehartmann97@gmail.com>
2023-01-17 22:54:50 +0300

    Upd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
+  parser: "@babel/eslint-parser",
   parserOptions: {
     babelOptions: {
-      configFile: './babel.config.json',
-    }
+      configFile: "./babel.config.json",
+    },
   },
   env: {
     browser: true,
     node: true,
-    es6: true
+    es6: true,
   },
-  extends: ['eslint:recommended', 'google'],
+  extends: ["eslint:recommended", "google"],
   rules: {
-    'semi': 'off',
-    'comma-dangle': 'off',
-    'require-jsdoc': 'off',
-    'linebreak-style': 'off',
-    'object-curly-spacing': 'off'
-  }
-}
+    semi: "off",
+    "comma-dangle": "off",
+    "require-jsdoc": "off",
+    "linebreak-style": "off",
+    "object-curly-spacing": "off",
+    quotes: [2, "double", { avoidEscape: true }],
+    indent: "off",
+  },
+};
